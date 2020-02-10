45cd9575ff0eb250bf7e28e87c2be0c54bc7ce01
Timothy Chun <40304454+timchuu@users.noreply.github.com>
2020-02-09 22:09:51 -0800

    Added redux to project

--- lesson-1-master/src/App.js
+++ lesson-1-master/src/App.js
@@ -4,40 +4,37 @@ import Shop from './pages/shop/Shop'
 import Header from './components/header/Header'
 import SignInSignUpPage from './pages/sign_in_sign_up/SignInSignUp'
 import { Switch, Route } from "react-router-dom";
+import { connect } from 'react-redux'
 import { auth, createUserProfileDocument } from './firebase/firebase.utils'
+import { setCurrentUser } from './redux/user/user_actions'
 import './App.css';
 
 
 class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      currentUser: null
-    }
-  }
+
 
   unsubscribeFromAuth = null
 
   componentDidMount() {
+    const { setCurrentUser } = this.props
 
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
       if (userAuth) {
         const userRef = await createUserProfileDocument(userAuth);
 
         userRef.onSnapshot(snapShot => {
-          this.setState({
-            currentUser: {
-              id: snapShot.id,
-              ...snapShot.data()
-            }
+          setCurrentUser({
+
+            id: snapShot.id,
+            ...snapShot.data()
+
           })
-          console.log(this.state)
+
         })
 
-      } else {
-        this.setState({ currentUser: userAuth })
-      }
 
+      }
+      setCurrentUser(userAuth);
     })
   }
 
@@ -47,7 +44,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        <Header currentUser={this.state.currentUser} />
+        <Header />
         <Switch>
           <Route exact path='/' component={Homepage} />
           <Route exact path='/shop' component={Shop} />
@@ -58,4 +55,8 @@ class App extends Component {
   }
 }
 
-export default App;  
+const mapDispatchToProps = dispatch => ({
+  setCurrentUser: user => dispatch(setCurrentUser(user))
+})
+
+export default connect(null, mapDispatchToProps)(App);  
---
+++ lesson-1-master/src/redux/root_reducer.js
@@ -0,0 +1,7 @@
+import { combineReducers } from 'redux'
+import userReducer from './user/user_reducer'
+
+export default combineReducers({
+
+    user: userReducer
+})
