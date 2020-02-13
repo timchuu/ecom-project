8289c1dba5185757eb9527d54d4d48c3c0901e49
Timothy Chun <40304454+timchuu@users.noreply.github.com>
2020-02-12 21:13:29 -0800

    added functionality to cart and selectors thru reselect pakage

--- lesson-1-master/src/components/cart_dropdown/CartDropdown.js
+++ lesson-1-master/src/components/cart_dropdown/CartDropdown.js
@@ -1,14 +1,25 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import { selectCartItems } from '../../redux/cart/cart_selectors'
+import CartItem from '../cart_item/CartItem'
 import CustomButton from '../custom_button/CustomButton'
 import './cart_dropdown.scss'
 
-const CartDropdown = () => {
+const CartDropdown = ({ cartItems }) => {
     return (
         <div className='cart-dropdown'>
-            <div className='cart-items' />
+            <div className='cart-items' >
+                {
+                    cartItems.map(cartItem => <CartItem key={cartItem.id} item={cartItem} />)
+                }
+            </div>
             <CustomButton>GO TO CHECKOUT</CustomButton>
         </div>
     )
 }
 
-export default CartDropdown
+const mapStateToProps = (state) => ({
+    cartItems: selectCartItems(state)
+})
+
+export default connect(mapStateToProps)(CartDropdown)
